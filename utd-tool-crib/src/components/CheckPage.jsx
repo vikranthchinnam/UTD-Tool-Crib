193728777b61996a62a94a9b32e5b262008349eb
vikranthchinnam <vikranth.chinnam@gmail.com>
2023-03-09 11:20:19 -0600

    Create react app

---
+++ utd-tool-crib/src/components/CheckPage.jsx
@@ -0,0 +1,21 @@
+import "./css/CheckPage.css"
+
+function CheckPage(){
+    return(
+        <div>
+            <div className="header-div">
+                <div className="back-button-div">
+                    <button className="back-button">Back</button>
+                </div>
+                <div className="title-div">
+                    <p className="title">Checkout/Check-in</p>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default CheckPage;
