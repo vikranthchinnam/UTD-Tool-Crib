b83a767ba0f3e95104d94d07e372131c47b93513
Farhin-Rashid <123594031+Farhin-Rashid@users.noreply.github.com>
2023-03-30 15:28:17 -0500

    Edited submit button to BorrowTools page

--- utd-tool-crib/src/components/BorrowTool.jsx
+++ utd-tool-crib/src/components/BorrowTool.jsx
@@ -1,28 +1,39 @@
 import {Link} from "react-router-dom"
 import "../styles/header.css"
 import "../styles/BorrowTools.css"
+import { useState } from "react";
 
 function BorrowTool(){
+
+    const [teamNum, setTeamNum] = useState('');
+    const [teamMember, setTeamMember] = useState('');
+    const [notes, setNotes] = useState('');
+    const [dueDate, setDueDate] = useState('');
+    const [dueTime, setDueTimes] = useState('');
+
     return(
-        <><div className="borrow-tools">
-        <div className="header">
-            <p className="title">Borrow Tool</p>
-            <Link to="/"><button>Back</button></Link>
-        </div>
-        <div className="input-box">
-            <label>Team Number</label>
-            <input type="text" placeholder="type here"/>
-            <label>Team Member</label>
-            <input type="text" placeholder="type here"/>
-            <label id="notes">Notes</label>
-            <input type="text" placeholder="type here"/>
-            <div className="date">
-            <label className="notes">Due Date</label>
-            <input type="text" placeholder="MM-DD-YYYY" id="due-date"/>
-            <label className="notes">Due Time</label>
-            <input type="text" />
+        <>
+            <div className="borrow-tools">
+                <div className="header">
+                    <p className="title">Borrow Tool</p>
+                    <Link to="/"><button>Back</button></Link>
+                </div>
+                <div className="input-box">
+                    <label>Team Number</label>
+                    <input type="text" placeholder="Type here" onChange = {(event) => setTeamNum(event.target.value)}/>
+                    <label>Team Member</label>
+                    <input type="text" placeholder="Type here" onChange = {(event) => setTeamMember(event.target.value)}/>
+                    <label id="notes">Notes</label>
+                    <input type="text" placeholder="Type here" onChange = {(event) => setNotes(event.target.value)}/>
+                    <div className="date">
+                        <label>Due Date</label>
+                        <input type="text" placeholder="MM-DD-YYYY" id="due-date" onChange = {(event) => setDueDate(event.target.value)}/>
+                        <label>Due Time</label>
+                        <input type="text" onChange = {(event) => setDueTimes(event.target.value)}/>
+                    </div>
+                    <center><button className="submit">Submit</button></center>
+                </div>
             </div>
-        </div>
-        </div></>
+        </>
     )};
 export default BorrowTool;
