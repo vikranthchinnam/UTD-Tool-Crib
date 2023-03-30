4aeea7887490406a2e04262927295eb14803ea5d
Farhin-Rashid <123594031+Farhin-Rashid@users.noreply.github.com>
2023-03-30 09:20:08 -0500

    Completed BorrowTools page

--- utd-tool-crib/src/components/BorrowTool.jsx
+++ utd-tool-crib/src/components/BorrowTool.jsx
@@ -1,16 +1,28 @@
 import {Link} from "react-router-dom"
 import "../styles/header.css"
+import "../styles/BorrowTools.css"
 
 function BorrowTool(){
     return(
-        <div>
-            <div className="header">
-                <p className="title">Borrow Tool</p>
-
-                <Link to="/"><button>Back</button></Link>
-                
+        <><div className="borrow-tools">
+        <div className="header">
+            <p className="title">Borrow Tool</p>
+            <Link to="/"><button>Back</button></Link>
+        </div>
+        <div className="input-box">
+            <label>Team Number</label>
+            <input type="text" placeholder="type here"/>
+            <label>Team Member</label>
+            <input type="text" placeholder="type here"/>
+            <label id="notes">Notes</label>
+            <input type="text" placeholder="type here"/>
+            <div className="date">
+            <label className="notes">Due Date</label>
+            <input type="text" placeholder="MM-DD-YYYY" id="due-date"/>
+            <label className="notes">Due Time</label>
+            <input type="text" />
             </div>
         </div>
-    );
-}
+        </div></>
+    )};
 export default BorrowTool;
